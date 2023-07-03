627c893f852e4de6a17b58da32f58f3462cbf002
paradiselots <130690632+paradiselots@users.noreply.github.com>
2023-07-03 16:31:03 -0400

    adding counter and calculator app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Counter from './Components/Calculator/Counter/Counter';
+import Calculator from './Components/Calculator/Calculator';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <br></br>
+      <hr></hr>
+      <br></br>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Calculator/Counter/Counter.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './Counter.css'
+
+
+
+const Counter = () => {
+  // Declare a state variable named "count" and set its initial value to 0
+  const [count, setCount] = useState(0);
+
+  // Functions to handle button click and update the count
+  const incrementCount = () => {
+    if (count >= 100) {
+      setCount(0);
+    } else {
+      setCount(count + 1);
+    }
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetButton = () => {
+    setCount(0);
+  };
+
+  const multiplyBy10 = () => {
+    if (count >= 100) {
+      setCount(0);
+    } else {
+      setCount(count * 10);
+    }
+  };
+
+  return (
+    <div className='counter'>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+      <button onClick={multiplyBy10}>Multiply by 10</button>
+      <button onClick={resetButton}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
